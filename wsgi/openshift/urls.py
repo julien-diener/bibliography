2754af073d534b6b1632077e7ee345ecae5c680c
julien-diener <julien.diener@gmail.com>
2014-12-01 17:33:52 +0100

    1st templage page

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,7 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    
+    url(r'^user/', 'views.publication', name='publication'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render_to_response
 
 def home(request):
      return render_to_response('home/home.html')
+     
+def publication(request):
+    var = {}
+    var['content'] = "not implemented"
+    var['user'] = 'zq7m2E8AAAAJ'
+    return render_to_response(request, 'main.html', var)
