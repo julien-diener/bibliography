7d800892609d188f4f54cadb4549b6e0f697bac7
julien-diener <julien.diener@gmail.com>
2014-12-02 12:27:06 +0100

    1st parsing

---
+++ wsgi/openshift/gcitation.py
@@ -0,0 +1,49 @@
+"""
+Read google scholar (registered) user citation 
+"""
+
+from urllib import FancyURLopener
+from bs4 import SoupStrainer, BeautifulSoup
+import re
+
+
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
+
+
+class Query(object):
+    title_filter = '<title>(.*)</title>'
+    pub_filter = ['tr',{'class':'gsc_a_tr'}]
+    
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.url = "http://scholar.google.fr/citations?user="+self.user_id
+        ##possible todo: &cstart=0&pagesize=20
+
+        self.html = MyOpener().open(self.url).read()
+
+
+        self.title = re.findall(Query.title_filter,self.html)[0]
+        
+        pubTree   = BeautifulSoup(self.html, parse_only=SoupStrainer(*Query.pub_filter))
+        
+        publication = []
+        for pub in pubTree.findAll(*Query.pub_filter):
+            tmp = list(pub.children.next().children)
+            p = {}
+            if len(tmp)>0: 
+                p['title'] = tmp[0].string
+                p['href']  = "http://scholar.google.fr/" + tmp[0].get('href')
+            if len(tmp)>1: 
+                p['authors'] = tmp[1].string
+            if len(tmp)>2:
+                tmp2 = list(tmp[2].children)
+                if len(tmp2)>0: p['journal'] = tmp2[0]
+                if len(tmp2)>1: p['year']    = tmp2[1].string.strip(' ,')
+            publication.append(p)
+        
+        self.publication = publication
+        
+    def __repr__(self):
+        return repr(self.publication)
+    
--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.http import HttpResponse
 
+import gcitation
+
 def home(request):
      return render_to_response('home/home.html')
      
 def publication(request):
     var = {}
-    var['content'] = "not implemented"
     var['user'] = 'zq7m2E8AAAAJ'
+    
+    
+    query = gcitation.Query(var['user'])
+    var['title'] = query.title
+    var['publication'] = query.publication
+    #var['content'] = "not implemented\n\n" + repr(query)
+    
     
     t = loader.get_template('main.html')
     c = Context(var)
