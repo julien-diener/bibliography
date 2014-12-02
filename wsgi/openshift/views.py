8ae670cf5d4be40660c5b472767f9e525a3dcb5c
julien-diener <julien.diener@gmail.com>
2014-12-02 09:56:45 +0100

    1st user page

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.template import Context, loader
+from django.http import HttpResponse
 
 def home(request):
      return render_to_response('home/home.html')
@@ -7,4 +9,10 @@ def publication(request):
     var = {}
     var['content'] = "not implemented"
     var['user'] = 'zq7m2E8AAAAJ'
+    
+    t = loader.get_template('main.html')
+    c = Context(var)
+    
+    return HttpResponse(t.render(c))
+    
     return render_to_response(request, 'main.html', var)
